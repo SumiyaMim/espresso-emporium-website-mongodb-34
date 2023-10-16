a84b1a64978241b09b0ee1e634ddb3cc769e1d07
SumiyaMim <sumiyaislam1093@gmail.com>
2023-10-16 15:50:50 +0600

    react, firebase, mongodb, tailwind, auth, sweetalert, crud

--- espresso-emporium-client/src/firebase/firebase.config.js
+++ espresso-emporium-client/src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDRr4v-AMQY6keEO840lYalh7ZnyNB9Q4c",
-  authDomain: "espresso-emporium-212d6.firebaseapp.com",
-  projectId: "espresso-emporium-212d6",
-  storageBucket: "espresso-emporium-212d6.appspot.com",
-  messagingSenderId: "177751157116",
-  appId: "1:177751157116:web:98e63839afaa5c11f75cf9"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
